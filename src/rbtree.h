57c553371c70bea72e9f167cdc3db0ab7cd88494
rigyeonghong <zxcvbee@naver.com>
2022-10-26 20:12:01 +0900

    feat: rbtree

--- src/rbtree.h
+++ src/rbtree.h
@@ -1,6 +1,8 @@
 #ifndef _RBTREE_H_
 #define _RBTREE_H_
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
@@ -19,14 +21,22 @@ typedef struct {
 } rbtree;
 
 rbtree *new_rbtree(void);
+void postorder_del(rbtree *, node_t *);
 void delete_rbtree(rbtree *);
 
+void left_rotate(rbtree *, node_t *);
+void right_rotate(rbtree *, node_t *);
+void insert_fixup(rbtree *, node_t *);
 node_t *rbtree_insert(rbtree *, const key_t);
+node_t *find_min(const rbtree *, node_t *);
 node_t *rbtree_find(const rbtree *, const key_t);
 node_t *rbtree_min(const rbtree *);
 node_t *rbtree_max(const rbtree *);
 int rbtree_erase(rbtree *, node_t *);
+void transplant(rbtree *, node_t *, node_t *);
+void del_fixup(rbtree *, node_t *);
 
 int rbtree_to_array(const rbtree *, key_t *, const size_t);
-
-#endif  // _RBTREE_H_
+// void preorder_search(rbtree *, node_t *);
+int inorder_search(const rbtree *, node_t *, int, key_t *, int);
+#endif // _RBTREE_H_
